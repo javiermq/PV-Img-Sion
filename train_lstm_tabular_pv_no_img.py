45b92bc6f301b4b0152293594df4d183dc0cafb7
javiermq <30752552+javiermq@users.noreply.github.com>
2026-06-09 10:29:50 +0200

    Update train_lstm_tabular_pv_no_img.py

--- train_lstm_tabular_pv_no_img.py
+++ train_lstm_tabular_pv_no_img.py
@@ -20,9 +20,9 @@ TSV_PATH = Path("data/weather_with_images.tsv")
 W = 8
 
 BATCH_SIZE = 16
-EPOCHS = 50
+EPOCHS = 25
 LR = 1e-3
-TRAIN_RATIO = 0.9
+
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
